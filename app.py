99e7a2ebdf496bf719e8aced9fec7b1d50399fe5
chen <chenzhang8722@gmail.com>
2022-07-25 12:09:11 -0400

    switch to socket

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import requests
 # Use the package we installed
 from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 # Initializes your app with your bot token and signing secret
 app = App(
@@ -86,4 +87,6 @@ def greetings(ack, say):
 
 # Start your app
 if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
+    handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
+    handler.start()
+
